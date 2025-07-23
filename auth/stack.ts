894bfd12dca4082e28c0ee42285e5a45881d0bb9
Pratik <prateekraiger123@gmail.com>
2025-07-23 15:18:47 +0530

    updated auth

--- auth/stack.ts
+++ auth/stack.ts
@@ -2,11 +2,10 @@ import { StackClientApp } from "@stackframe/react";
 import { useNavigate } from "react-router-dom";
 
 export const stackClientApp = new StackClientApp({
-  projectId: "9e5a717e-5280-4671-9e3b-35561dcd49d5",
-  publishableClientKey: "pck_pn2hfm4v8k0bwvk8ytvk20ttr6gacfw2rkpa7pq76m4t8",
+  projectId: import.meta.env.VITE_STACK_PROJECT_ID,
+  publishableClientKey: import.meta.env.VITE_STACK_PUBLISHABLE_CLIENT_KEY,
   tokenStore: "cookie",
   redirectMethod: {
     useNavigate,
   },
-  redirectUrl: "/chat", // Redirect to chat page after authentication
 });
